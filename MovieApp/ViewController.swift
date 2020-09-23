b25362e2ba4b4f1c592a57382a1ef01c229dc36d
Jitendra Rajput <jitendra.rajput@in10media.com>
2020-09-24 00:58:58 +0530

    Initial Commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by EPIC on 24/09/20.
+//  Copyright © 2020 Movie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
